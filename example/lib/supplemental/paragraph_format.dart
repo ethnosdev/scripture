e5c7c4310dc8312a53112a2fe3dee789ac5eddf0
ethnosdev <contact@ethnos.dev>
2025-10-01 17:12:46 +0800

    paragraph format types

---
+++ example/lib/supplemental/paragraph_format.dart
@@ -0,0 +1,49 @@
+enum ParagraphFormat {
+  /// margin, no indentation
+  m,
+
+  /// break, blank vertical space
+  b,
+
+  /// poetry indentation level 1
+  q1,
+
+  /// poetry indentation level 2
+  q2,
+
+  /// Embedded text opening
+  pmo,
+
+  /// list item level 1
+  li1,
+
+  /// list item level 2
+  li2,
+
+  /// centered
+  pc,
+
+  /// right aligned
+  qr,
+
+  /// Descriptive Title (Psalms "Of David")
+  d,
+
+  /// Cross Reference
+  r,
+
+  /// Section Heading Level 1
+  s1,
+
+  /// Section Heading Level 2
+  s2,
+
+  /// major section (Psalms)
+  ms,
+
+  /// major section range (Psalms)
+  mr,
+
+  /// Acrostic Heading (Psalm 119)
+  qa,
+}
